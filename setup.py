da33afc8f4edb732029feefab0a8cc3dd80525bd
손량 <loop.infinitely@gmail.com>
2018-11-17 15:13:14 +0900

    fixed a bug in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+import setuptools
+
 from setuptools import setup
+from os import path
 
 py_modules = [
     'requests',
@@ -6,22 +9,24 @@ py_modules = [
     'xdg'
 ]
 
+here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='boj-tool',
-      version='v1.0.0',
+      version='1.0.0',
       description='A tool for submitting to BOJ',
       long_description=long_description,
       long_description_content_type='text/markdown',
       author='Ryang Sohn',
       author_email='loop.infinitely@gmail.com',
-      install_requirements=py_modules,
+      install_requires=py_modules,
       entry_points={
           'console_scripts': [
-              'boj=boj_tool:main'
+              'boj=boj:main'
           ]
       },
+      packages=setuptools.find_packages(),
       project_urls={
           'Bug Reports': 'https://github.com/sohnryang/boj-tool/issues',
           'Source': 'https://github.com/sohnryang/boj-tool'
